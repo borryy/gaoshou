b85d131ec4337c49a82948bba5d03f14c8f0959f
borroy <763780082@qq.com>
2019-06-27 18:12:50 +0800

    11

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -11,182 +11,154 @@
 			</view>
 		</view>
 		<view class="main">
-			<scroll-view scroll-y="true" class="scroll-Y" >
+			<scroll-view scroll-y="true" class="scroll-Y scroll-Y-L" >
 				<view class="scroll-view-item listTitle mainBackColor">高守施工</view>
-				<view class="scroll-view-item listm" :class="titleId == item.id?'active':''" @click="changeItem(item.id)" v-for="(item,index) in mainList[0].children" :key="index">{{item.title}}</view>
+				<view class="scroll-view-item listm" :class="titleId == item.id?'active':''" @click="changeItem(item.id,item.pid)" v-for="item in mainList1" :key="item.id">{{item.title}}</view>
 				<view class="scroll-view-item listTitle mainBackColor">高守精选</view>
-				<view class="scroll-view-item listm" :class="titleId == item.id?'active':''" @click="changeItem(item.id)" v-for="(item,index) in mainList[1].children" :key="index">{{item.title}}</view>
+				<view class="scroll-view-item listm" :class="titleId == item.id?'active':''" @click="changeItem(item.id,item.pid)" v-for="item in mainList2" :key="item.id">{{item.title}}</view>
 			</scroll-view>
-			<scroll-view scroll-y="true" class="scroll-Y-R" >
-				<view class="rightList">
+			<scroll-view scroll-y="true" class="scroll-Y scroll-Y-R">
+				<view class="scroll-view-item rightList" v-for="(item,index) in lists" :key="index">
 					<view class="tipImg">
 						<image class="imgs" src="/static/icon/head.png" mode=""></image>
 					</view>
 					<view class="msg">
 						<view class="tit">
-							清包美缝服务
+							{{item.title}}
 						</view>
 						<view class="small">
-							只包含人工费，每小时30元
+							{{item.small}}
 						</view>
 					</view>
-					<view class="add">
-						<uni-icon type="plus" color="#ca0c16" size="20"></uni-icon>
-						<input type="number" value="0" />
-						<uni-icon type="minus" color="#ca0c16" size="20"></uni-icon>
-					</view>
-				</view>
-				<view class="rightList">
-					<view class="tipImg">
-						<image class="imgs" src="/static/icon/head.png" mode=""></image>
-					</view>
-					<view class="msg">
-						<view class="tit">
-							清包美缝服务
-						</view>
-						<view class="small">
-							只包含人工费，每小时30元
-						</view>
-					</view>
-					<view class="add">
-						<uni-icon type="plus" color="#ca0c16" size="20"></uni-icon>
-						<input type="number" value="0" />
-						<uni-icon type="minus" color="#ca0c16" size="20"></uni-icon>
-					</view>
-				</view>
-				<view class="rightList">
-					<view class="tipImg">
-						<image class="imgs" src="/static/icon/head.png" mode=""></image>
-					</view>
-					<view class="msg">
-						<view class="tit">
-							清包美缝服务
-						</view>
-						<view class="small">
-							只包含人工费，每小时30元
-						</view>
-					</view>
-					<view class="add">
-						<uni-icon type="plus" color="#ca0c16" size="20"></uni-icon>
-						<input type="number" value="0" />
-						<uni-icon type="minus" color="#ca0c16" size="20"></uni-icon>
-					</view>
-				</view>
-				<view class="rightList">
-					<view class="tipImg">
-						<image class="imgs" src="/static/icon/head.png" mode=""></image>
-					</view>
-					<view class="msg">
-						<view class="tit">
-							清包美缝服务
-						</view>
-						<view class="small">
-							只包含人工费，每小时30元
-						</view>
-					</view>
-					<view class="add">
-						<uni-icon type="plus" color="#ca0c16" size="20"></uni-icon>
-						<input type="number" value="0" />
-						<uni-icon type="minus" color="#ca0c16" size="20"></uni-icon>
-					</view>
-				</view>
-				<view class="rightList">
-					<view class="tipImg">
-						<image class="imgs" src="/static/icon/head.png" mode=""></image>
-					</view>
-					<view class="msg">
-						<view class="tit">
-							清包美缝服务
-						</view>
-						<view class="small">
-							只包含人工费，每小时30元
-						</view>
-					</view>
-					<view class="add">
-						<uni-icon type="plus" color="#ca0c16" size="20"></uni-icon>
-						<input type="number" value="0" />
-						<uni-icon type="minus" color="#ca0c16" size="20"></uni-icon>
-					</view>
-				</view>
-				<view class="rightList">
-					<view class="tipImg">
-						<image class="imgs" src="/static/icon/head.png" mode=""></image>
-					</view>
-					<view class="msg">
-						<view class="tit">
-							清包美缝服务
-						</view>
-						<view class="small">
-							只包含人工费，每小时30元
-						</view>
-					</view>
-					<view class="add">
-						<uni-icon type="plus" color="#ca0c16" size="20"></uni-icon>
-						<input type="number" value="0" />
-						<uni-icon type="minus" color="#ca0c16" size="20"></uni-icon>
-					</view>
-				</view>
-				<view class="rightList">
-					<view class="tipImg">
-						<image class="imgs" src="/static/icon/head.png" mode=""></image>
-					</view>
-					<view class="msg">
-						<view class="tit">
-							清包美缝服务
-						</view>
-						<view class="small">
-							只包含人工费，每小时30元
-						</view>
-					</view>
-					<view class="add">
-						<uni-icon type="plus" color="#ca0c16" size="20"></uni-icon>
-						<input type="number" value="0" />
-						<uni-icon type="minus" color="#ca0c16" size="20"></uni-icon>
-					</view>
-				</view>
-				<view class="rightList">
-					<view class="tipImg">
-						<image class="imgs" src="/static/icon/head.png" mode=""></image>
-					</view>
-					<view class="msg">
-						<view class="tit">
-							清包美缝服务
-						</view>
-						<view class="small">
-							只包含人工费，每小时30元
-						</view>
-					</view>
-					<view class="add">
-						<uni-icon type="plus" color="#ca0c16" size="20"></uni-icon>
-						<input type="number" value="0" />
-						<uni-icon type="minus" color="#ca0c16" size="20"></uni-icon>
+					<view class="add" @click="showBottom()">
+						<image class="addimg" src="/static/icon/shop2.png" mode=""></image>
 					</view>
 				</view>
 			</scroll-view>
+			
 		</view>
 	</view>
 </template>
 
 <script>
 	import {uniIcon} from '@dcloudio/uni-ui'
+	import {uniPopup} from "@dcloudio/uni-ui"
 	export default {
-		components: {uniIcon},
+		components: {uniIcon,uniPopup},
 		data() {
 			return { 
 				titleId: '',
+				mainList1:[],
+				mainList2:[],
+				lists:[],
+				showPopupBottomShare:false,
 				mainList:[
 					{
 						"title":"高守施工",
 						"children":[
 							{
 								"id":"1",
+								"pid":"1",
 								"title":"高守美缝",
-								"children":[]
+								"children":[
+									{
+										"id":"100",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									},
+									{
+										"id":"101",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									},
+									{
+										"id":"102",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									},
+									{
+										"id":"103",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									},
+									{
+										"id":"104",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									},
+									{
+										"id":"105",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									},
+									{
+										"id":"106",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									},
+									{
+										"id":"107",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									},
+									{
+										"id":"108",
+										"title":"清包美缝服务",
+										"small":"只包含人工费，每小时30元"
+									}
+								]
 							},
 							{
 								"id":"2",
+								"pid":"1",
 								"title":"高守防水",
-								"children":[]
+								"children":[
+									{
+										"id":"100",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									},
+									{
+										"id":"101",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									},
+									{
+										"id":"102",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									},
+									{
+										"id":"103",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									},
+									{
+										"id":"104",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									},
+									{
+										"id":"105",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									},
+									{
+										"id":"106",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									},
+									{
+										"id":"107",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									},
+									{
+										"id":"108",
+										"title":"清包防水服务",
+										"small":"只包含人工费，每小时100元"
+									}
+								]
 							}
 						]
 					},
@@ -195,52 +167,44 @@
 						"children":[
 							{
 								"id":"3",
+								"pid":"2",
 								"title":"美缝剂胶",
 								"children":[]
 							},
 							{
 								"id":"4",
+								"pid":"2",
 								"title":"防水用品",
 								"children":[]
 							},
 							{
-								"id":"3",
+								"id":"5",
+								"pid":"2",
 								"title":"美缝剂胶",
 								"children":[]
 							},
 							{
-								"id":"4",
+								"id":"6",
+								"pid":"2",
 								"title":"防水用品",
 								"children":[]
 							},
 							{
-								"id":"3",
+								"id":"7",
+								"pid":"2",
 								"title":"美缝剂胶",
 								"children":[]
 							},
 							{
-								"id":"4",
+								"id":"8",
+								"pid":"2",
 								"title":"防水用品",
 								"children":[]
 							},
 							{
-								"id":"3",
+								"id":"9",
+								"pid":"2",
 								"title":"美缝剂胶",
-								"children":[]
-							},
-							{
-								"id":"4",
-								"title":"防水用品",
-								"children":[]
-							},
-							{
-								"id":"3",
-								"title":"美缝剂胶",
-								"children":[]
-							},
-							{
-								"id":"4",
-								"title":"防水用品",
 								"children":[]
 							}
 						]
@@ -248,26 +212,45 @@
 				]
 			}
 		},
-		onLoad() {
-			this.titleId = this.mainList[0].children[0].id
+		mounted() {
+			this.titleId = this.mainList[0].children[0].id;
+			this.mainList1 = this.mainList[0].children;
+			this.mainList2 = this.mainList[1].children; 
+			var that = this
+			this.lists = this.mainList1.filter(function(item){
+				return item.id == that.titleId
+			})[0].children
+			console.log(this.lists)
 		},
 		methods: {
-			con:function(){
-				uni.showLoading({
-					title:"loading..."
-				})
+			showBottom:function(){
+				this.showPopupBottomShare = !this.showPopupBottomShare
 			},
-			changeItem:function(id){
+			hidePopup:function(){
+				
+			},
+			changeItem:function(id,pid){
 				this.titleId = id
+				if(pid == 1){
+					this.lists = this.mainList1.filter(function(item){
+						return item.id == id
+					})[0].children
+				}else{
+					this.lists = this.mainList2.filter(function(item){
+						return item.id == id
+					})[0].children
+				}
 			},
 			go:function(){ 
-				
+				this.titleId = this.mainList[0].children[0].id;
+				this.mainList1 = this.mainList[0].children;
+				this.mainList2 = this.mainList[1].children;
 			}
 		}
 	}
 </script>
 
-<style>
+<style scoped>
 	
 	.topMsg{
 		display: flex;
@@ -314,7 +297,7 @@
 		overflow: hidden;
 		border-top: 1px solid #FFFFFF;
 	}
-	.scroll-Y{
+	.scroll-Y-L{
 		width: 25%;
 		position: absolute;
 		top: 223upx;
@@ -323,17 +306,17 @@
 		left: 0;
 		padding-bottom: 100upx;
 	}
-	.scroll-Y .scroll-view-item{
+	.scroll-Y-L .scroll-view-item{
 		text-align: center;
 		padding: 30upx 0;
 		font-size: 28upx;
 	}
-	.scroll-Y .scroll-view-item.active{
+	.scroll-Y-L .scroll-view-item.active{
 		background-color: #FFFFFF;
 		color: #ca0c16;
 		position: relative;
 	}
-	.scroll-Y .scroll-view-item.active::before{
+	.scroll-Y-L .scroll-view-item.active::before{
 		content: '';
 		position: absolute;
 		border-right: 2px solid #ca0c16;
@@ -341,7 +324,7 @@
 		left: 10upx;
 		top: 34upx;
 	}
-	.scroll-Y .listTitle{
+	.scroll-Y-L .listTitle{
 		padding: 20upx 0;
 		font-size: 30upx;
 		border-top: 1px solid #FFFFFF;
@@ -354,6 +337,7 @@
 		right: 0;
 		background-color: #FFFFFF;
 		padding-bottom: 100upx;
+		white-space:nowrap;
 	}
 	.scroll-Y-R .rightList{
 		font-size: 28upx;
@@ -391,13 +375,9 @@
 		flex-wrap: nowrap;
 		align-items:flex-end;
 	}
-	.scroll-Y-R .rightList .add input{
-		height: 43upx!important;
-		min-height: 43upx;
-		font-size: 24upx;
-		text-align: center;
-	}
-	.scroll-Y-R .rightList .add .uni-icon{
-		font-size: 36upx!important;
+	
+	.scroll-Y-R .rightList .add .addimg{
+		width: 30upx;
+		height: 30upx;
 	}
 </style>
